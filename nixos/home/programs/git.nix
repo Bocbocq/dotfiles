b37dbd748d24bfcc74051d4938f0e8109dbe23ed
Bocbocq <anbocque@gmail.com>
2026-02-01 15:24:20 +0000

    re add lazygit

--- nixos/home/programs/git.nix
+++ nixos/home/programs/git.nix
@@ -1,9 +1,9 @@
-{ pkgs, ... }: {
-  home.packages = with pkgs; [ lazygit delta ];
-
-  programs.git = {
-    enable = true;
-    userName = "Bocbocq";
-    userEmail = "anbocque@gmail.com";
-  };
+{
+  pkgs,
+  ...
+}: {
+  home.packages = with pkgs; [
+    lazygit
+    delta
+  ];
 }
