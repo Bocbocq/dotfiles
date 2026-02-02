83cd1a681c032fe8bd797e28430457bbd51bd3b3
Bocbocq <anbocque@gmail.com>
2026-02-02 11:17:36 +0000

    fix internet on home-server

--- nixos/hosts/media-server/configuration.nix
+++ nixos/hosts/media-server/configuration.nix
@@ -1,25 +1,28 @@
 # Edit this configuration file to define what should be installed on
 # your system. Help is available in the configuration.nix(5) man page, on
 # https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
-
 # NixOS-WSL specific options are documented on the NixOS-WSL repository:
 # https://github.com/nix-community/NixOS-WSL
-
-{ config, lib, pkgs, username, ... }:
-
 {
+  config,
+  lib,
+  pkgs,
+  username,
+  ...
+}: {
   nixpkgs.config.allowUnfree = true;
 
-  imports = [ /etc/nixos/hardware-configuration.nix ];
+  imports = [./hardware-configuration.nix];
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
   system.stateVersion = "24.11";
 
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix.settings.experimental-features = ["nix-command" "flakes"];
+  nix.settings.trusted-users = ["root" "boc"];
 
-  environment.systemPackages = with pkgs; [ git vim wget docker ];
+  environment.systemPackages = with pkgs; [git vim wget docker];
 
   environment.variables.EDITOR = "vim";
 
@@ -29,7 +32,7 @@
     shell = pkgs.zsh;
     isNormalUser = true;
     group = "users";
-    extraGroups = [ "wheel" "networkmanager" "docker" ];
+    extraGroups = ["wheel" "networkmanager" "docker"];
   };
 
   security.sudo.enable = true;
@@ -40,13 +43,12 @@
 
   hardware.enableRedistributableFirmware = true;
   hardware.enableAllFirmware = true;
-  boot.initrd.kernelModules = [ "i915" ];
+  boot.initrd.kernelModules = ["i915"];
 
-  hardware.firmware = with pkgs; [ linux-firmware ];
+  hardware.firmware = with pkgs; [linux-firmware];
 
-  boot.kernelModules = [ "i915" ];
-  boot.kernelParams =
-    [ "i915.enable_guc=3" "i915.fastboot=1" "i915.force_probe=46d4" ];
+  boot.kernelModules = ["i915"];
+  boot.kernelParams = ["i915.enable_guc=3" "i915.fastboot=1" "i915.force_probe=46d4"];
 
   hardware.opengl.enable = true;
   hardware.graphics = {
@@ -61,15 +63,16 @@
     ];
   };
 
-  networking.useDHCP = false;
-  networking.interfaces.enp1s0 = {
-    useDHCP = false;
-    ipv4.addresses = [{
-      address = "192.168.1.26";
-      prefixLength = 24;
-    }];
-  };
-  networking.defaultGateway = "192.168.1.1";
-  networking.nameservers = [ "1.1.1.1" "8.8.8.8" ];
+  networking.useDHCP = true;
+  # networking.interfaces.enp1s0 = {
+  #   useDHCP = false;
+  #   ipv4.addresses = [
+  #     {
+  #       address = "192.168.1.26";
+  #       prefixLength = 24;
+  #     }
+  #   ];
+  # };
+  # networking.defaultGateway = "192.168.1.1";
+  # networking.nameservers = [ "1.1.1.1" "8.8.8.8" ];
 }
-
