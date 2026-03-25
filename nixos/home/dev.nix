1c0b89679c1de1bcd2e44655fb4253a8d25ae88b
anthony-bocquet_rimes <anthony.bocquet@rimes.com>
2026-03-25 20:34:41 +0100

    refactor: add home/dev.nix consolidating language toolchains

---
+++ nixos/home/dev.nix
@@ -0,0 +1,38 @@
+{pkgs, ...}: {
+  home.packages = with pkgs; [
+    # Python
+    python312
+    uv
+    poetry
+    black
+    isort
+    python3Packages.flake8
+    ruff
+    basedpyright
+    pyright
+    mypy
+
+    # Lua
+    lua
+    stylua
+    lua-language-server
+    luajitPackages.luarocks
+
+    # Rust
+    cargo
+
+    # JavaScript
+    nodejs
+
+    # Nix
+    nixd
+    alejandra
+    statix
+    deadnix
+
+    # Markdown / common dev tools
+    markdownlint-cli2
+    markdown-toc
+    marksman
+  ];
+}
